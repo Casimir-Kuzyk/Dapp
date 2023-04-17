9909d8dc9d259893a216d44c9ba789ee24aea4a2
Casimir-Kuzyk <casimirkuzyk@gmail.com>
2023-04-17 15:22:12 -0700

    css styling, connect to address or wallet

--- bundle.js
+++ bundle.js
@@ -12,6 +12,8 @@ console.log('hey');
 // const axios = require('axios/dist/browser/axios.cjs')
 //add a button that when clicked, it connects the users MetaMask wallet, and displays the eth balance and wallet address
 
+const addressButton = document.getElementById('address-button');
+const addressInput = document.getElementById('address-input');
 const walletButton = document.getElementById('myButton');
 const ethBalanceDiv = document.getElementById('ethBalance');
 const ethAddressDiv = document.getElementById('ethAddress');
@@ -19,6 +21,54 @@ const ethBlockDiv = document.getElementById('ethBlock');
 const ethGasDiv = document.getElementById('ethGas');
 const ethChainDiv = document.getElementById('ethChain');
 const ethNetworkDiv = document.getElementById('ethNetwork');
+
+//button to connect to input address account
+
+addressButton.addEventListener("click", function () {
+  var address = addressInput.value;
+  window.ethereum.request({
+    method: "eth_getBalance",
+    params: [address, "latest"]
+  }).then(balance => {
+    ethBalanceDiv.innerText = parseInt(balance);
+  });
+  window.ethereum.request({
+    method: "eth_gasPrice",
+    params: []
+  }).then(gasPrice => {
+    ethGasDiv.innerText = _ethers.ethers.utils.formatEther(gasPrice);
+  });
+  window.ethereum.request({
+    method: "eth_blockNumber",
+    params: []
+  }).then(blockNumber => {
+    ethBlockDiv.innerText = parseInt(blockNumber);
+  });
+
+  //use API to gather NFTs from the address
+  //first use axios to make an http request to the alchemy API
+  const apiKey = "hpJuc6-oDz05yb8ORLdDoerArgsEFOtt";
+  const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTs/`;
+  const testWallet = address;
+  const urlStruct = `${baseURL}?owner=${testWallet}`;
+  var config = {
+    method: 'get',
+    url: urlStruct
+  };
+  (0, _axios.default)(config).then(function (response) {
+    for (var i = 0; i < response.data.ownedNfts.length; i++) {
+      var nft = response.data.ownedNfts[i];
+      var img = document.createElement('img');
+      img.src = nft.metadata.image;
+      img.classList.add('nftImage');
+      document.getElementById('nftContainer').appendChild(img);
+    }
+    ;
+  });
+});
+
+//button to connect to users metamask account
+
 walletButton.addEventListener("click", function () {
   //provider is an ethereum provider, and we can create it with window.ethereum, which is how we access metamask in code.
   //window.ethereum object is available if a user has a metamask installed.
@@ -29,52 +79,47 @@ walletButton.addEventListener("click", function () {
     // console.log(addresses);
     const address = addresses[0];
     console.log(address);
-    ethAddressDiv.value = address.toString();
+    ethAddressDiv.innerText = address.toString();
     window.ethereum.request({
       method: "eth_getBalance",
       params: [address, "latest"]
     }).then(balance => {
-      ethBalanceDiv.value = parseInt(balance);
+      ethBalanceDiv.innerText = parseInt(balance);
     });
     window.ethereum.request({
       method: "eth_gasPrice",
       params: []
     }).then(gasPrice => {
-      ethGasDiv.value = _ethers.ethers.utils.formatEther(gasPrice);
+      ethGasDiv.innerText = _ethers.ethers.utils.formatEther(gasPrice);
     });
     window.ethereum.request({
       method: "eth_blockNumber",
       params: []
     }).then(blockNumber => {
-      ethBlockDiv.value = parseInt(blockNumber);
+      ethBlockDiv.innerText = parseInt(blockNumber);
     });
 
     //use API to gather NFTs from the address
     //first use axios to make an http request to the alchemy API
     const apiKey = "hpJuc6-oDz05yb8ORLdDoerArgsEFOtt";
     const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTs/`;
-    const testWallet = '0xe257b12d6746705ce3caeb24f9079bded18fb54f';
+    const testWallet = address;
     const urlStruct = `${baseURL}?owner=${testWallet}`;
     var config = {
       method: 'get',
       url: urlStruct
     };
     (0, _axios.default)(config).then(function (response) {
+      document.getElementById('nftContainer').innerHTML = "";
       for (var i = 0; i < response.data.ownedNfts.length; i++) {
         var nft = response.data.ownedNfts[i];
         var img = document.createElement('img');
         img.src = nft.metadata.image;
-        document.getElementById('nftImages').appendChild(img);
+        img.classList.add('nftImage');
+        document.getElementById('nftContainer').appendChild(img);
       }
       ;
     });
-
-    //     .then(response => console.log(JSON.stringify(response.data,null,2)))
-    //     .catch(error => console.log(error));
-
-    // myAxios.get(url).then(function(response){ //response is the whole object.. we can use.data to just get what we need
-    //     console.log(response.data)
-    // });
   });
 });
 
